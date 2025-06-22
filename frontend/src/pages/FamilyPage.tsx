3dd7c42295e91db0a696a40eeae3f480f64ba79c
Sami Touil <samitouil@macstudiodesami.home>
2025-06-22 02:12:55 +0200

    fix: remove unused useTranslation import in FamilyPage
    - Remove unused 't' variable and useTranslation import
    - Fixes TypeScript build error: TS6133
    - Frontend build now passes successfully

--- frontend/src/pages/FamilyPage.tsx
+++ frontend/src/pages/FamilyPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { FamilyManagement } from '../components/FamilyManagement';
 import { UserSummaryCard } from '../components/UserSummaryCard';
 import './pages.css';
 
 const FamilyPage: React.FC = () => {
-  const { t } = useTranslation();
-
   return (
     <div className="tasks-page">
       <div className="family-layout">
